609c4fce8edef35211acbd12076fa9c7c9e03d61
Geremsa <geremsanrzr@gmail.com>
2022-09-23 20:18:50 +0530

    ui

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,13 +5,33 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        'Barlow': ['Barlow Condensed', 'regular']
+        'Barlow': ['Barlow Condensed', 'regular'],
+        'Bellefair': ['Bellefair', 'sans sarif']
       },
       backgroundImage: {
         'background': "url('../assets/home/background-home-desktop.jpg')"
       },
       margin: {
         '12.5px': '3.5rem',
+      },
+      fontSize: {
+        '2.5xl': '1.75rem',
+        '10xl': '9.3rem'
+      },
+      colors: {
+        'custom-blue': '#d0d6f9',
+      },
+      width: {
+        '108': '28.125rem'
+      },
+      height: {
+        '86': '23rem',
+      },
+      letterSpacing: {
+        'custom-widest': '0.297rem'
+      },
+      inset: {
+        '86': '23rem',
       }
     },
   },
